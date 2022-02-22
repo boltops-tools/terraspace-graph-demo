b933b329c1029d7d47cf7dded3cff35ebd4408ca
Tung Nguyen <tongueroo@gmail.com>
2022-02-22 18:23:43 +0000

    use s3 backend

--- config/terraform/backend.tf
+++ config/terraform/backend.tf
@@ -1,2 +1,12 @@
-# Local backend is used the sake of the demo and speed.
-# That's why there's no backend configured.
+# You might want to comment out and use local backend for sake of speed.
+# But if you clear out the .terraspace-cache the local terraform.tfstate is lost.
+# Using s3 backend so wont lose the terraform.tfstate.
+terraform {
+  backend "s3" {
+    bucket         = "<%= expansion('terraform-state-:ACCOUNT-:REGION-:ENV') %>"
+    key            = "<%= expansion(':REGION/:ENV/:BUILD_DIR/terraform.tfstate') %>"
+    region         = "<%= expansion(':REGION') %>"
+    encrypt        = true
+    dynamodb_table = "terraform_locks"
+  }
+}
